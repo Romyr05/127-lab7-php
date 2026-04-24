6f84c8e0c708d559af117c2e2770595b5fbe2119
Tupe <jhonchriztopher123@gmail.com>
2026-04-24 21:33:07 +0800

    added/proper constraints to attributes

--- students.sql
+++ students.sql
@@ -1,16 +1,16 @@
 -- Student Details
 CREATE TABLE students (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    Name VARCHAR(40),
-    Age INT CHECK (Age >= 0 AND Age <= 99),
-    Email VARCHAR(40),
-    Course VARCHAR(40)
+    Name VARCHAR(40) NOT NULL,
+    Age INT NOT NULL CHECK (Age >= 0 AND Age <= 99),
+    Email VARCHAR(40) NOT NULL UNIQUE,
+    Course VARCHAR(40) NOT NULL
 );
 
 -- Academic details /linked to students
 CREATE TABLE student_details (
     detail_id INT AUTO_INCREMENT PRIMARY KEY,
-    student_id INT,
+    student_id INT NOT NULL,
     YearLevel INT CHECK (YearLevel IN (1,2,3,4)),
     GraduationStatus BOOLEAN,
     ImagePath VARCHAR(255),
