6db58b8bfdebe055378dae97d2ac8c6bdc220308
Romyr <romyrlopez123@gmail.com>
2026-05-04 14:46:12 +0800

    a

--- DBConnector.php
+++ DBConnector.php
@@ -1,10 +1,11 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "student-test"; //DB name
+$servername = getenv('DB_HOST') ?: "localhost";
+$username = getenv('DB_USERNAME') ?: (getenv('DB_USER') ?: "root");
+$password = getenv('DB_PASSWORD') ?: "";
+$dbname = getenv('DB_NAME') ?: "student-test"; // DB name
+$port = (int) (getenv('DB_PORT') ?: 3306);
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
